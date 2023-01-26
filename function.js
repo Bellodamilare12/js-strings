1978b87efa038ade44176bd137f43053f578b4d9
Bello Damilare <dammyfrosh290@gmail.com>
2023-01-26 11:39:55 +0100

    first commit

---
+++ function.js
@@ -0,0 +1,22 @@
+function combString(str){
+    var lenStr = str.length;
+    var result = [];
+    var indexCurrent = 0;
+
+    while(indexCurrent < lenStr){
+        var char = str.charAt(indexCurrent);
+        var x;
+        var arrTemp = [char];
+
+        for(x in result) {
+            arrTemp.push(""+result[x]+char);
+        }
+        result = result.concat(arrTemp);
+
+        indexCurrent++;
+    }
+
+    return result;
+}
+
+console.log(combString("abc"));
